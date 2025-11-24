a4ac3d24a0075920d8bab6ec98fb17531cfe99b7
CitrusTime <ajanidbrown96@gmail.com>
2025-11-23 23:33:11 -0500

    Updated intial starter files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,22 @@
-/* Add your JavaScript to this file */
+function isEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    const form = document.querySelector('.newsletter form');
+    const messageDiv = document.querySelector('.newsletter .message');
+    
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let emailInput = document.getElementById('email');
+        let email = emailInput.value.trim();
+        
+        if (isEmail(email)) {
+            messageDiv.innerHTML = "<p>Thank you! Your email address '" + email + "' has been added to our mailing list!</p>";
+            emailInput.value = '';
+        } else {
+            messageDiv.innerHTML = "<p>Please enter a valid email address.</p>";
+        }
+    });
+});
